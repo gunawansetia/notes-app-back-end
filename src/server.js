f2f5c14aa87108bc4b8cd901912af911cb0a6372
Gunawan Setia <56891835+gunawansetia@users.noreply.github.com>
2025-03-08 17:02:34 +0700

    fix wrong folder

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes.js');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 8001,
+    // eslint-disable-next-line no-undef
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  });
+
+  console.log('ada di', routes);
+  server.route(routes);
+  await server.start();
+
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
